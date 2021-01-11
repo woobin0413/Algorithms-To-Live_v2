dad523020804c4d1e1effd232bde7048e59825be
woobin0413 <woobin0413@gmail.com>
2021-01-10 20:08:08 -0500

    update :)

--- src/Leetcode/Shuffle_the_Array.java
+++ src/Leetcode/Shuffle_the_Array.java
@@ -1,11 +1,9 @@
 package Leetcode;
 
-import java.util.Iterator;
-
 public class Shuffle_the_Array {
 
 	public int[] shuffle(int[] nums, int n) {
-		
+
 //		
 //		int newNumArr [] = new int[nums.length];
 //		int count = 0;
@@ -23,21 +21,21 @@ public class Shuffle_the_Array {
 //		return newNumArr;
 //		
 //		
-		int[] res = new int[2*n];
-		for(int i = 0; i < n; i++){
-		   res[2 * i] = nums[i]; //even
-		   res[2 * i + 1] = nums[n+i]; //odd
+		int[] res = new int[2 * n];
+		for (int i = 0; i < n; i++) {
+			res[2 * i] = nums[i]; // even
+			res[2 * i + 1] = nums[n + i]; // odd
 		}
 		return res;
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int nums [] = new int[]{2,5,1,3,4,7};
+		int nums[] = new int[] { 2, 5, 1, 3, 4, 7 };
 		Shuffle_the_Array s1 = new Shuffle_the_Array();
 		s1.shuffle(nums, 3);
-		System.out.println(5/2);
+		System.out.println(5 / 2);
 	}
 
 }
