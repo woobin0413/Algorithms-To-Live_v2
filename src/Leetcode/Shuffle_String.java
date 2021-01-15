f3e660077630b890cf69eff1817ddba8aa9c3156
woobin0413 <woobin0413@gmail.com>
2021-01-14 22:20:28 -0500

    https://leetcode.com/problems/shuffle-string/
    String.valueOf(charArray);

---
+++ src/Leetcode/Shuffle_String.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Shuffle_String {
+
+	public static void restoreString(String s, int[] indices) {
+//	    TreeMap<Integer,Character> map =new TreeMap<Integer,Character>();
+//		
+//		if(s.length() != indices.length) {
+//			return "";
+//		}
+//		
+//		for (int i = 0; i < indices.length; i++) {
+//			map.put(indices[i], s.charAt(i));
+//		}
+//		String result = "";
+//		for (int i = 0; i < indices.length; i++) {
+//			result += map.get(i);
+//		}
+//		
+//		return result;
+		
+		char[] charArray = new char[s.length()];
+		for(int i = 0; i < indices.length; i++) {
+			int pos = indices[i];
+			charArray[pos] = s.charAt(i);
+			
+		}
+		System.out.println(String.valueOf(charArray));
+		
+	}
+
+	public static void main(String[] args) {
+		String s = "codeleet";
+		int[] indices = new int[] { 4, 5, 6, 7, 0, 2, 1, 3 };
+		restoreString(s,indices);
+	}
+}
