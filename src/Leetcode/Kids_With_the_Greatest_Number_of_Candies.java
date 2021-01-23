e2a2671ea280d9a3b928380088adbdd52e0648ce
woobin0413 <woobin0413@gmail.com>
2021-01-22 22:26:42 -0500

    minor fixes

--- src/Leetcode/Kids_With_the_Greatest_Number_of_Candies.java
+++ src/Leetcode/Kids_With_the_Greatest_Number_of_Candies.java
@@ -1,8 +1,6 @@
 package Leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Kids_With_the_Greatest_Number_of_Candies {
