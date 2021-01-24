cebf15cdc927e1072b3d57f2b97d28c59b6424d7
woobin0413 <woobin0413@gmail.com>
2021-01-24 03:07:01 -0500

    https://leetcode.com/problems/xor-operation-in-an-array

---
+++ src/Leetcode/XOR_Operation_in_an_Array.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class XOR_Operation_in_an_Array {
+	public static void xorOperation(int n, int start) {
+
+		int res = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			res = start + 2 * i ^ res;
+
+		}
+		System.out.println(res);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		xorOperation(1, 7);
+	}
+
+}
