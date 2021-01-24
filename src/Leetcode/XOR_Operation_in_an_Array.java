4849d86730f9f0ae13f3e5d665c18e93647a6135
woobin0413 <woobin0413@gmail.com>
2021-01-24 13:03:01 -0500

    minor fix

--- src/Leetcode/XOR_Operation_in_an_Array.java
+++ src/Leetcode/XOR_Operation_in_an_Array.java
@@ -4,18 +4,19 @@ public class XOR_Operation_in_an_Array {
 	public static void xorOperation(int n, int start) {
 
 		int res = 0;
-
+		
 		for (int i = 0; i < n; i++) {
-
-			res = start + 2 * i ^ res;
-
+		
+			res = start + 2*i ^ res;
+			
+			
 		}
 		System.out.println(res);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		xorOperation(1, 7);
+		xorOperation(1,7);
 	}
 
 }
